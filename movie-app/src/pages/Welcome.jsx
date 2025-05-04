36170a73f64249885e541c60a34b53bde22031e1
Buse <buseboylan48@gmail.com>
2025-05-04 21:16:57 +0300

    commit

--- movie-app/src/pages/Welcome.jsx
+++ movie-app/src/pages/Welcome.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Welcome() {
   return (
-    <div className='welcome-background'>Welcome</div>
+    <div className='welcome-background'>
+        
+    </div>
   )
 }
 
