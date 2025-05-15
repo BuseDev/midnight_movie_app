511bbcd39fcadc035215a6a6fbbc825acfd3b95a
Buse <buseboylan48@gmail.com>
2025-05-16 01:06:11 +0300

    updated

--- movie-app/src/pages/Welcome.jsx
+++ movie-app/src/pages/Welcome.jsx
@@ -13,10 +13,7 @@ function Welcome() {
       <div className="welcome-underline"></div>
 
       <p className="welcome-desc">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        Discover trending, hidden gems, and all-time favorites — tailored just for you. Your next movie night starts here.
       </p>
 
       <button className="welcome-button" onClick={() => navigate("/Home")}>
